769aaca78f87e0bf5efdc2f0748c783c57c06971
fadelanumah <fadelanumah@gmail.com>
2025-04-12 21:08:31 +0700

    feat(users): add default board for fresh user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,33 @@
+import { boardRepository } from '../repositories/board.repository';
 import { userRepository } from '../repositories/user.repository';
+import { CreateBoardInput } from '../schema/board.schema';
 import { CreateUserInput } from '../schema/user.schema';
 
 export const userService = {
   create: async (data: CreateUserInput) => {
-    return userRepository.create(data);
+    const newUser = await userRepository.create(data);
+
+    // create default board for new user
+    const defaultBoard: CreateBoardInput[] = [
+      {
+        name: "Todo",
+        user_id: newUser.id
+      },
+      {
+        name: "On Going",
+        user_id: newUser.id
+      },
+      {
+        name: "Completed",
+        user_id: newUser.id
+      }
+    ];
+
+    const createdBoard = await boardRepository.createMany(defaultBoard);
+
+    return newUser;
   },
-  
+
   getById: async (userId: number) => {
     return userRepository.getById(userId);
   }
