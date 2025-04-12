a620f8ffa5e259a5e7311d82b3343572e2796ef0
fadelanumah <fadelanumah@gmail.com>
2025-04-12 13:12:24 +0700

    fix(users): fix password not hash after create user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { createUserSchema } from '../schema/user.schema';
 import { userService } from '../services/user.servie';
+import { hash } from 'bcrypt';
 
 export const userController = {
   create: async(req: Request, res: Response, next: NextFunction) => {
     try {
       const data = createUserSchema.parse(req.body);
+      data.password = await hash(data.password, 12)
       const createdUser = await userService.create(data);
       const { password, ...safeUser } = createdUser;
       res.status(201).json({
@@ -14,12 +16,12 @@ export const userController = {
       })
     } catch (error: any) {
       if (error.name === "ZodError") {
-        res.status(400).json({errors: error.errors})
+        res.status(400).json({error})
       }
       if (error.code === "P2002") {
-        res.status(400).json({errors: error.message})
+        res.status(400).json({error})
       }
-      res.status(500).json({message: error.message})
+      res.status(500).json({error})
       next(error)
     }
   }
