33f78210ec494fb06577a1d66d781f1c86231962
fadelanumah <fadelanumah@gmail.com>
2025-04-13 00:10:55 +0700

    feat(error-utils): add condition to handle fkey error

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -7,6 +7,9 @@ export const checkError = (res: Response, next: NextFunction, error: any) => {
   if (error.code === "P2002") {
     res.status(409).json({ message: "Duplicate field value", meta: error.meta });
   }
+  if (error.code === "P2003") {
+    res.status(409).json({ message: "You can't delete this item because there is item relate to it", meta: error.meta });
+  }
   if (error.code === "P2025") {
     res.status(404).json({ message: "Record not found", meta: error.meta });
   }
