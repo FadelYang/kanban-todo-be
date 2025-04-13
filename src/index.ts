e62069ecf08b416dcb14fec3082c3a28f04f47e8
fadelanumah <fadelanumah@gmail.com>
2025-04-13 23:23:26 +0700

    fix: fix cors issue

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,22 @@ import { PrismaClient } from "../generated/prisma";
 import express, { Request, Response } from "express";
 import createError from "http-errors";
 import router from './routes';
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 export const prisma = new PrismaClient();
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use(express.json());
 
 app.use(cookieParser());
 
-app.use("/api/v1", router)
+app.use("/api/v1", router);
 
 app.use((req: Request, res: Response, next: Function) => {
   next(createError(404));
@@ -19,4 +25,4 @@ app.use((req: Request, res: Response, next: Function) => {
 
 app.listen(3001, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:3001`);
-})
+});
