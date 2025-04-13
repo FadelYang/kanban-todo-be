9690c50319d20c3ba71dd89b9ea1e9da58f9260a
fadelanumah <fadelanumah@gmail.com>
2025-04-14 01:34:03 +0700

    fix(boards): fix get detail board data

--- src/routes/board.route.ts
+++ src/routes/board.route.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from '../middlewares/auth.middleware';
 
 const boardRouter = Router();
 
-boardRouter.get("/", authMiddleware, boardController.getDetail);
+boardRouter.get("/:board_id", authMiddleware, boardController.getDetail);
 boardRouter.post("/", authMiddleware, boardController.create);
 boardRouter.put("/:board_id", authMiddleware, boardController.update);
 boardRouter.delete("/:board_id", authMiddleware, boardController.delete);
